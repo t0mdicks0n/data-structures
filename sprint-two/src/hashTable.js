5da25d2cf0f327ee142880542160ac52493c94db
Hack Reactor Student <student@hackreactor.com>
2017-03-31 16:38:43 -0700

    Added first part of hash table.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,44 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(key, value) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+
+  if (this._storage.get(index) === undefined) {
+  	this._storage.set(index, []);
+  }
+
+  this._storage.get(index).push([key, value]);
 };
 
-HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.retrieve = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+ 
+  var value;
+  if (this._storage.get(index) !== undefined) {
+	  this._storage.get(index).forEach(function(element, index, array) {
+	  	if (element[0] === key) {
+	  		value = element[1];
+	  	}
+	  });
+  }
+  return value;
 };
 
-HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.remove = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+
+  var idx;
+  if (this._storage.get(index) !== undefined) {
+  	this._storage.get(index).forEach(function(element, index, array) {
+  		if (element[0] === key) {
+  			idx = index;
+  		}
+  	});
+  	this._storage.get(index).splice(idx, 1);
+  }
 };
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  var truth = false;
+  this.storage.forEach(function(el){
+  	if (el === item){
+  		truth = true; 
+  	}
+  })
+  return truth;
 };
 
 setPrototype.remove = function(item) {
+  var idx;
+  this.storage.forEach(function(el, index, array){
+  	if (el === item){
+  		idx = index 
+  	}
+  })
+  this.storage.splice(idx, 1);
 };
 
 /*
